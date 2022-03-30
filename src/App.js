2c1701290360953e2ce62dfb0a7fb6ddbed38f96
Ashutosh-kr7 <102675804+Ashutosh-kr7@users.noreply.github.com>
2022-03-30 19:36:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import ClassComponent from "./Components/ClassComponent";
+import FunctionalComponent from "./Components/FunctionalComponent";
+
+function App() {
+  return (
+    <>
+      <ClassComponent />
+      <FunctionalComponent />
+    </>
+  );
+}
+
+export default App;
